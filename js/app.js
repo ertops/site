d9da34a2936a7d149a176618f74ba4199319e0c3
Stefanos Kornilios Mitsis Poiitidis <skmp@ertops.com>
2013-06-19 11:27:04 +0300

    Slightly better async handling

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 
+//guess repo from url, default to ertops
 gh_repo = document.location.href.match( /[^\/]*\/\/([^\\]*).github.io\/([^\/]*)/);
 gh_repo = gh_repo ? gh_repo[1] + "/" + gh_repo[2] : "ertops/site";
+gh_repo_fragments = undefined;
 
 function load_fragment(name) {
 	var list = $('.masthead ul .active'),
@@ -17,26 +19,44 @@ function load_fragment(name) {
 	});
 }
 
-$(document).ready(function() {
-	load_fragment('Home');
-});
+function populate_pages(data) {
+    var list = $('.masthead ul');
+    if (list.length) {
+      list.empty();
+      _.filter(data, function(it) { 
+        return it.name.indexOf('.html')!=-1 
 
+      }).map(function(it) { 
+        var name = it.name.substr(0,it.name.length-5);
+        list.append(
+          '<li id="tab-' + name + '"><a href="#' + name + '">' + name + '</a></li>'
+        );
+        $("#tab-" + name).click(function(w) {
+          load_fragment(name);
+        });
+      });
+      
+      load_fragment('Home');
+      
+      return true;
+    }
+    else
+      return false;
+}
 
+//fetch the nav list
+//Fragment loading and navlist could be done in parallel, but aren blocking right now
 $.ajax('https://api.github.com/repos/' + gh_repo + '/contents/pages/', { 
 	success: function(data) { 
-		var list = $('.masthead ul');
-		list.empty();
-		_.filter(data, function(it) { 
-			return it.name.indexOf('.html')!=-1 
-
-		}).map(function(it) { 
-			var name = it.name.substr(0,it.name.length-5);
-			list.append(
-				'<li id="tab-' + name + '"><a href="#' + name + '">' + name + '</a></li>'
-			);
-			$("#tab-" + name).click(function(w) {
-				load_fragment(name);
-			});
-		});
+		if (!populate_pages(data))
+      gh_repo_fragments=data; //.masthead not there, do it on ready()
 	}
 });
+
+$(document).ready(function() {
+  if (gh_repo_fragments) {
+    populate_pages(gh_repo_fragments);
+    delete gh_repo_fragments;
+  }
+});
+
